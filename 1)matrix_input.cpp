41fa6c3611ace3fca24933f51e69eeac85153741
Aakash Choudhari <aakash.choudhari.btech2024@sitpune.edu.in>
2025-08-06 15:33:08 +0530

    Create 1)matrix_input.cpp

---
+++ 1)matrix_input.cpp
@@ -0,0 +1,31 @@
+// Aakash Choudhari
+// E&TC-A2
+// 24070123031 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+    cout << "Enter rows and columns: ";
+    cin >> rows >> cols;
+
+    int matrix[10][10];
+
+    cout << "Enter elements:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+
+    cout << "Matrix is:\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
